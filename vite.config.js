cc32ecc4776b34c2fe02cec71b37de65f11cb5fc
Amrit Kumar <118508137+PirateKing55@users.noreply.github.com>
2024-07-24 09:12:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
+   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome'],
-    },
+      external: ['@fortawesome/free-brands-svg-icons']
+    }
   },
 });
